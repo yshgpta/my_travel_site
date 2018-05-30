0ae42a26d31c168e09feeb2be0cb50b0330c5000
Yash <ysh.gpta8333@gmail.com>
2018-05-30 11:13:18 +0530

    Some unimportant changes

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
+
+
+var config = {
+  mode:{
+    css:{
+      render:{
+        css:{
+          template:'./gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+}
+
+gulp.task('createSprite', function(){
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+  .pipe(svgSprite(config))
+  .pipe(gulp.dest('./app/temp/sprite/'))
+});
+
+gulp.task('copySpriteCSS',['createSprite'] function(){
+  return gulp.src('./app/temp/sprite/css/*.css')
+  .pipe(rename('_sprite.css'))
+  .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons',['createSprite','copySpriteCSS']);
+})
