c733f192f97109f6c13afc25ec8c20c928082ac5
Yash <ysh.gpta8333@gmail.com>
2018-05-31 23:52:31 +0530

    Completed styling of our header of mobile view

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,9 +19,17 @@ gulp.task('watch',function(){
   watch('./app/assets/styles/**/*.css',function(){
     gulp.start('cssInject')
   })
-})
+
+  watch('./app/assets/scripts/**/*.js',function(){
+    gulp.start('scriptsRefresh');
+  })
+});
 
 gulp.task('cssInject',['styles'],function(){
   gulp.src('./app/temp/styles/styles.css')
   .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh',['scripts'], function(){
+  browserSync.reload();
 })
